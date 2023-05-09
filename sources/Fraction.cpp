134aa53d4e86adabcb5ac73d2ff92345e4312075
LielYoash <lielyoash76@gmail.com>
2023-05-09 19:42:44 +0300

    working +,-,++,--

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -1,1 +1,114 @@
-hello
+#include <iostream>
+#include <stdexcept>
+#include "Fraction.hpp"
+using namespace std;
+using namespace ariel;
+
+//----------------------------------------------------------------------------------//
+//                                   Constructors                                    
+//----------------------------------------------------------------------------------//
+
+Fraction ::Fraction(int numerator, int denominator)
+{
+    if (denominator == 0)
+    {
+        throw std::invalid_argument("Denominator can't be 0");
+    }
+    this->numerator = numerator;
+    this->denominator = denominator;
+}
+
+Fraction ::Fraction(int numerator)
+{
+    this->numerator = numerator;
+    this->denominator = 1;
+}
+
+Fraction ::Fraction()
+{
+    this->numerator = 0;
+    this->denominator = 1;
+}
+
+//----------------------------------------------------------------------------------//
+//                                Getters and Setters                                
+//----------------------------------------------------------------------------------//
+
+int Fraction ::getNumerator() const
+{
+    return this->numerator;
+}
+
+int Fraction ::getDenominator() const
+{
+    return this->denominator;
+}
+
+void Fraction ::setNumerator(int numerator)
+{
+    this->numerator = numerator;
+}
+
+void Fraction ::setDenominator(int denominator)
+{
+    if (denominator == 0)
+    {
+        throw std::invalid_argument("Denominator can't be 0");
+    }
+    this->denominator = denominator;
+}
+
+//----------------------------------------------------------------------------------//
+//                                      Methods                                      
+//----------------------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------------------//
+//                 Operators:        +         -        ++        --                 
+//----------------------------------------------------------------------------------//
+Fraction Fraction ::operator+(const Fraction &other)
+{
+    int newNumerator = (this->numerator * other.getDenominator()) + (other.getNumerator() * this->denominator);
+    int newDenominator = this->denominator * other.getDenominator();
+    int gcd = findGCD(newNumerator,newDenominator);
+    return Fraction(newNumerator/gcd, newDenominator/gcd);
+}
+
+Fraction Fraction ::operator-(const Fraction &other)
+{
+    int newNumerator = (this->numerator * other.getDenominator()) - (other.getNumerator() * this->denominator);
+    int newDenominator = this->denominator * other.getDenominator();
+    int gcd = abs(findGCD(newNumerator,newDenominator));
+    return Fraction(newNumerator/gcd, newDenominator/gcd);
+}
+
+Fraction Fraction ::operator++(int num){
+    Fraction temp(this->numerator, this->denominator);
+    this->numerator += this->denominator;
+    return temp;
+}
+
+Fraction& Fraction ::operator++(){
+    this->numerator += this->denominator;
+    return *this;
+}
+
+Fraction Fraction ::operator--(int num){
+    Fraction temp(this->numerator, this->denominator);
+    this->numerator -= this->denominator;
+    return temp;
+}
+
+Fraction& Fraction ::operator--(){
+    this->numerator -= this->denominator;
+    return *this;
+}
+
+//----------------------------------------------------------------------------------//
+//                                Operator:        ==                                
+//----------------------------------------------------------------------------------//
+
+Fraction Fraction ::operator==(const Fraction &other)
+
+
+
--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -1,1 +1,73 @@
-sup
+#ifndef Fraction_HPP
+#define Fraction_HPP
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+using namespace std;
+
+namespace ariel
+{
+    class Fraction
+    {
+    private:
+        int numerator;
+        int denominator;
+
+    public:
+        Fraction(int numerator, int denominator);
+        Fraction(int numerator);
+        Fraction();
+        int getNumerator() const;
+        int getDenominator() const;
+        void setNumerator(int numerator);
+        void setDenominator(int denominator);
+        friend ostream &operator<<(ostream &os, const Fraction &f)
+        {
+            os << f.getNumerator() << "/" << f.getDenominator();
+            return os;
+        }
+        friend istream &operator>>(istream &is, Fraction &f)
+        {
+            int numerator;
+            int denominator;
+            char c;
+            is >> numerator >> c >> denominator;
+            if (c != '/')
+            {
+                throw std::invalid_argument("Invalid input");
+            }
+            f.setNumerator(numerator);
+            f.setDenominator(denominator);
+            return is;
+        }
+        int findGCD(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            return findGCD(b, a % b);
+        }
+        Fraction operator+(const Fraction &f);
+        friend Fraction operator+(int num, const Fraction &f)
+        {
+            Fraction temp(num);
+            return temp + f;
+        }
+        Fraction operator-(const Fraction &f);
+        friend Fraction operator-(int num, const Fraction &f)
+        {
+            Fraction temp(num);
+            return temp - f;
+        }
+        Fraction operator++(int);
+        Fraction &operator++();
+        Fraction operator--(int);
+        Fraction &operator--();
+
+    };
+}
+
+#endif /* Fraction_HPP */
